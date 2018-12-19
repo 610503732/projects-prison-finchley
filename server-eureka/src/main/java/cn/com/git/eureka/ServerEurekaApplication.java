6f9327af8e0eeff965154a374ef3c8ba4563b5af
zhangzhenfu <zzf610503732@163.com>
2018-12-19 10:38:26 +0800

    server-eureka

---
+++ server-eureka/src/main/java/cn/com/git/eureka/ServerEurekaApplication.java
@@ -0,0 +1,17 @@
+package cn.com.git.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//该注解表示开启注册中心
+@EnableEurekaServer
+@SpringBootApplication
+public class ServerEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerEurekaApplication.class, args);
+    }
+
+}
+
